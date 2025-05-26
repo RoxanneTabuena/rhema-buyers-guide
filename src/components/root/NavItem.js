f6809521954d2b49edc3eeef9eeab081aeda0789
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-26 11:32:18 -0700

    add hide option

--- src/components/root/NavItem.js
+++ src/components/root/NavItem.js
@@ -1,13 +1,17 @@
 import { NavLink } from 'react-router-dom'
 import { titleSequence } from '../content/articleDir'
 import { returnXL } from '../utils'
+import { getLinkHeight, formatHeight } from '../utils'
 import style from './root.module.css'
 
-export const NavItem = ({art, heightX}) => {
+export const NavItem = ({art, heightX, hideLink}) => {
 const title = titleSequence[art]
 let className = returnXL(heightX)? 
 `${style.link} ${heightX} link xl`:
 `${style.link} ${heightX} link`
+if(hideLink){
+    return <div style={{height: formatHeight(getLinkHeight())}}></div>
+}
 if(art === 'intro'){
     return (
         <NavLink 
