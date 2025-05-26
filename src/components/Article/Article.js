55f9b8cb4fb525666398a8ac4b06b4462c45bdfb
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-26 11:31:01 -0700

    add logic for article data fetching

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,8 +1,15 @@
 import style from './article.module.css'
+import {maintenance} from '../content/maintenance'
 export const Article = ({art}) => {
+    const content_dir = {
+        'maintenance': maintenance
+    }
     return (
         <article className={style.art}>
             <p>{art}</p>
+            {content_dir[art]&&
+            <p>{content_dir[art].content}</p>
+            }
         </article>
     )
 }
