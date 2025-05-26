902af872e92820e2d54b9564deb40a373e2bd0c6
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-26 11:34:46 -0700

    cleanup load and unload logic for streamlined ux

--- src/components/root/Viewport/Preview.js
+++ src/components/root/Viewport/Preview.js
@@ -1,32 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useVisibility } from '../../../hooks/useVisibility';
 import { NavItem } from "../NavItem";
-import { titleSequence } from "../../content/articleDir";
 import { miniRouter } from "./miniRouter";
-import { getNextArt, getLinkHeight, formatHeight} from "../../utils";
+import { getNextArt} from "../../utils";
+
 import style from '../root.module.css'
 
 export const Preview = ({cur, height, handlePreviewEnter, handlePreviewExit}) => {
+    const [ hideLink, setHideLink] = useState(false)
+    const [previewLoaded, setPreviewLoaded] = useState( false)
     const next = getNextArt(cur)
-    const linkHeight = !getLinkHeight() ? '5px' : formatHeight(getLinkHeight()*2)
-    const [previewRef, previewIsVisible] = useVisibility({
-      threshold: .01,
-      rootMargin: linkHeight
+    const [upperRef, upperIsVisible] = useVisibility({
+      threshold: 1,
     });
-  
-    useEffect(() => {
-      if (previewIsVisible) {
-        handlePreviewEnter()
-      } else {
-        handlePreviewExit()
-      }
-    }, [previewIsVisible]);
+    const [lowerRef, lowerIsVisible] = useVisibility({
+      threshold: 1,
+    });
 
+    useEffect(()=>{
+      if(!previewLoaded){
+        if(upperIsVisible){
+          handlePreviewEnter()
+          setPreviewLoaded(true)
+        }
+      }else{
+        if(!lowerIsVisible && upperIsVisible){
+          setHideLink(true)
+          handlePreviewExit()
+        }else if(!lowerIsVisible&& !upperIsVisible){
+          setPreviewLoaded(false)
+          setHideLink(false)
+        }else if(lowerIsVisible && hideLink){
+          setHideLink(false)
+        }
+      }
+    }, [upperIsVisible, lowerIsVisible])
     return (
-        <div ref={previewRef} style={{minHeight: height}}>
-            <NavItem art={next} heightX={style.xl}>
-            <h2>{titleSequence[next]}</h2>
-            </NavItem>
+        <div style={{minHeight: height}}>
+                <h2 className={style.hidden}>turkey</h2>
+            <div ref={upperRef} style={{ height: '1px' }} id="previewTitle"></div>
+              <NavItem art={next} heightX={style.xl} hideLink={hideLink}>
+              </NavItem>
+            <div ref={lowerRef} style={{ height: '1px' }}></div>
             <div className={style.preview}>
             {miniRouter[next]}
             </div>
