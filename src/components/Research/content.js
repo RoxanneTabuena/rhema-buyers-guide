6287ef70064d38068da4436080a0f2a45eda5978
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-30 11:34:48 -0700

    add research content document

---
+++ src/components/Research/content.js
@@ -0,0 +1,47 @@
+export const competition_dic= {
+    'ProCore Productions': {
+        src: `https://imagehostclub.s3.us-west-1.amazonaws.com/rhema/competition/proCore.png`,
+        href: `https://procoreproductions.com/product-category/audio-equipment/`,
+        good: [
+            `Searchable Inventory: They have used tags and categories to make the inventory searchable. `,
+            `Individual Item pages: Customers have all the information they need in an easily digestible format. There is also a secret bonus to this. More pages with relevant information will boost your site relevance for search engines.`,
+            `Included product Images: The majority of people intake visual information faster than any other delivery method. That’s why, even in the case of an audio company, visual media is much more than just a good-to-have. Its an industry standard.`,
+            `Information on pricing: Weather you handle things by-item, or in the form of a package, people are more confident when they know what they are getting themselves into. Don’t roll the dice, list the price! `
+        ],
+        bad: [
+            `Add a cart feature: Save some time for both your customers and your staff, by allowing a rental list feature to the site. Items of interest are compiled in a central place, until your customer is ready to reach out with an itemized list detailing their needs.`,
+            `Provide a more complete picture: Add Info about Renting Procedures so your customers know what to expect.`,
+            `Add a purchase option: Easily incorporate your used gear listings by adding a purchase option to relevant products.`
+        ]
+    },
+    'Theatrix':{
+        src: `https://imagehostclub.s3.us-west-1.amazonaws.com/rhema/competition/Theatrix.png`,
+        href: `https://www.theatrixhawaii.com/`,
+        good: [
+            `Quality Graphics: The Theatrix site does a good job of telling their story through pictures. Over here we see the family, and the team acting confident and professional. `,
+            `Organized Navigation: Services are organized by department, and while it would be nice to have more information on their process, what little they have provided works for the phone call.`,
+            `Consistent branding: Here we have a simple legible theme that conveys a tone of consistency throughout the site.`,
+            `Excellent Promo Video: Their attention here pays off. Its the first thing a user sees, and it does a good job of conveying the life of an event through the customers eyes.It delivers a strong message: Theatrix will take care of you.`
+        ],
+        bad: [
+            `Pay attention to navigation details: Recent Projects should be more than an Image gallery. Lets add services preformed, and accolades`,
+            `Put More Stock Into Inventory: The contents of your inventory is the primary concern of half of your business. Lets put together a complete and information rich guide for your customers`,
+            `Create a smoother mobile layout: The theatric mobile layout feels more like an afterthought.Ours should be easy to navigate, particularly for your rentals side of the program. Which people may often be accessing through their mobile devices due to unforeseen last-minute needs.`
+        ]
+    },
+    'Hawaii Stage': {
+        src: `https://imagehostclub.s3.us-west-1.amazonaws.com/rhema/competition/HawaiiStage.png`,
+        href: `https://www.hawaii-stage.com/`,
+        good: [
+            `Consistent Branding: An authentically local company that has its act together. They mad simple choices in their site theme, contrast really well with their video-heavy graphics.`,
+            `Custom banners for every page:gives site a professional and complete feel, while strengthening branding.`,
+            `Content Writing: Their business information is well written and laid out beautifully.`,
+            `Looks good on both Mobile and Desktop: Their site will leave a good impression on everyone, regardless of what device it is accessed from.`,
+        ],
+        bad: [
+            `Provide More Information on Rental Process and Inventory: Our site will have the advantage of not only a full inventory- but a streamlined rental process saving time for both your staff and your customers.`,
+            `Make service information more digestible: Use a list format for service information, rather than articles where company strengths and specialties get lost.`,
+            `Clean up graphics: Many people find constant movement off-putting when they are simply searching for information. Lets keep big assets like videos inside page banners, but simplify actual content by only using static images.`
+        ]
+    }
+}
