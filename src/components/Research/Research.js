e4c6aa589c78f2514383473fe33e20b05e3c376a
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-30 11:35:12 -0700

    attach list of competitors

--- src/components/Research/Research.js
+++ src/components/Research/Research.js
@@ -1,8 +1,19 @@
+import { useState, useEffect } from 'react'
+import { Competitor } from './Competetitor'
 import style from './research.module.css'
+import { competition_dic } from './content'
+import { keyFromStr, getArtHeight, formatHeight } from '../utils'
 export const Research = () => {
+    const [height, setHeight] = useState(null)
+    const comps = Object.keys(competition_dic)
+    useEffect(()=>{
+        setHeight(formatHeight(getArtHeight()*comps.length))
+    },[])
     return (
-        <div className={style.research}>
-            <p>research</p>
+        <div className={style.research} style={{minHeight: height}}>
+           { comps.map((comp, i)=>{
+            return <Competitor key={keyFromStr(comp, i)} name={comp} info={competition_dic[comp]} />
+           })}
         </div>
     )
 }
