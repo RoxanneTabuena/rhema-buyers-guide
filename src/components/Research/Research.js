3e1794cd8fa75c3409d1a0e89a03f87ce3853dd1
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-30 14:01:17 -0700

    delete obsolete code

--- src/components/Research/Research.js
+++ src/components/Research/Research.js
@@ -1,16 +1,12 @@
-import { useState, useEffect } from 'react'
 import { Competitor } from './Competetitor'
-import style from './research.module.css'
 import { competition_dic } from './content'
-import { keyFromStr, getArtHeight, formatHeight } from '../utils'
+import { keyFromStr } from '../utils'
+
+
 export const Research = () => {
-    const [height, setHeight] = useState(null)
     const comps = Object.keys(competition_dic)
-    useEffect(()=>{
-        setHeight(formatHeight(getArtHeight()*comps.length))
-    },[])
     return (
-        <div className={style.research} style={{minHeight: height}}>
+        <div>
            { comps.map((comp, i)=>{
             return <Competitor key={keyFromStr(comp, i)} name={comp} info={competition_dic[comp]} />
            })}
